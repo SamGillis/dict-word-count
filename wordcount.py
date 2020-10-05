9298bb099cf34dc98b7f1a162e785d045c9babbc
Erin Grady <erinmariegrady@gmail.com>
2020-10-05 21:37:02 +0000

    Alphabetically sorts by keys
    Co-authored-by: Sam Gillis <61370193+SamGillis@users.noreply.github.com>

--- wordcount.py
+++ wordcount.py
@@ -4,7 +4,7 @@ import collections
 
 # put your code here.
 ##implemented using dictionaries
-"""def word_count(text):
+def word_count(text):
     the_file = open(text)
     words_dict = {}
     for line in the_file:
@@ -19,36 +19,37 @@ import collections
         ##take off any punct
 
         ##add word to dictionary or update value
-    for each in words_dict:
-        print(f"{each} {words_dict[each]}")
-
-        ##testing configuration
-
-
-word_count(sys.argv[1])"""
-
-##implemented using counters
-
-def word_count(text):
-    the_file = open(text)
-    words_counter = collections.Counter()
-    # the_file = the_file.translate(str.maketrans('', '', string.punctuation))
-    the_file = the_file.read()
-    the_file = the_file.translate(str.maketrans('', '', string.punctuation))
-    the_file = the_file.replace('\n', ' ')
-    print(f"{the_file}")
-    list_words = the_file.split(' ')
-   
-    
-    for word in list_words:
-        word = word.lower()
-        words_counter[word] += 1
-
-        ##add word to dictionary or update value
-    for each in words_counter:
-        print(f"{each} {words_counter[each]}")
+    ##print(sorted(words_dict.values()))
+    for each in sorted(words_dict.keys()):
+        print(f'{each} {words_dict[each]}')
 
         ##testing configuration
 
 
 word_count(sys.argv[1])
+
+##implemented using counters
+
+# def word_count(text):
+#     the_file = open(text)
+#     words_counter = collections.Counter()
+#     # the_file = the_file.translate(str.maketrans('', '', string.punctuation))
+#     the_file = the_file.read()
+#     the_file = the_file.translate(str.maketrans('', '', string.punctuation))
+#     the_file = the_file.replace('\n', ' ')
+#     print(f"{the_file}")
+#     list_words = the_file.split(' ')
+   
+    
+#     for word in list_words:
+#         word = word.lower()
+#         words_counter[word] += 1
+
+#         ##add word to dictionary or update value
+#     for each in words_counter:
+#         print(f"{each} {words_counter[each]}")
+
+#         ##testing configuration
+
+
+# word_count(sys.argv[1])
